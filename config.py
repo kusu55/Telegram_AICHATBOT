f4d27805d0068d514792ec393c290230b9d68a46
KUSUMA GURRALA <116880167+kusu55@users.noreply.github.com>
2025-01-30 03:15:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
-import logging
-
 BOT_TOKEN = "Your_telegram_bot_token"
 MONGO_URI = "Your_mongo_db_connection_string"
 GEMINI_API_KEY = "Your_gemini_api_key"
 WEB_SEARCH_API = "Your_serp_api_key"
-
-logging.basicConfig(level=logging.INFO)
