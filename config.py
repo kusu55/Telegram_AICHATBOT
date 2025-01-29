3fac2f7e505ef08023f0867c20454ecb2db7bd0b
KUSUMA GURRALA <116880167+kusu55@users.noreply.github.com>
2025-01-30 03:11:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import logging
 
-BOT_TOKEN = "7382366521:AAE-BwSSCVzdKAlBrfCcTIAEcPuJVeVqlaM"
-MONGO_URI = "mongodb+srv://kusuma:8q5k4KuCcuFDfk43@cluster0.bl00q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-GEMINI_API_KEY = "AIzaSyBYwHeAPdgiPlcRMDUhlQqUk7sfZlQm09c"
-WEB_SEARCH_API = "5f6489ef53dafb427cce638bd06600bc343f301714a621d9dbc93ade3e9d4bcd"
+BOT_TOKEN = "Your_telegram_bot_token"
+MONGO_URI = "Your_mongo_db_connection_string"
+GEMINI_API_KEY = "Your_gemini_api_key"
+WEB_SEARCH_API = "Your_serp_api_key"
 
 logging.basicConfig(level=logging.INFO)
